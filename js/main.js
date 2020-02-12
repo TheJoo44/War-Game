97712d41bdc28b5c0cd84d415e918d6ae97943ac
Jeremy Birnbaum <thejoo44@yahoo.com>
2020-02-12 15:46:30 -0700

    Finish Win Logic

--- js/main.js
+++ js/main.js
@@ -53,7 +53,7 @@ const cardFace = document.createElement("img");
 playBtn.addEventListener("click", playCards);
 replayBtn.addEventListener("click", replay);
 bgmBtn.addEventListener("click", bgmOnOff);
-sfxBtn.addEventListener('click', autoPlay);
+// sfxBtn.addEventListener('click', autoPlay);
 
 /*----------Functions--------------*/
 
@@ -179,105 +179,105 @@ function playComputerCard(isWar) {
 // console.log(computerCard)
 
 // compares the value of each card(NOT WORKING) Plays until one player has all cards
-// function compareCards() {
-//   // if (computerDeck.length === 0 || playerDeck.length === 0) {
-//   //   gmWinner()
-//   /*} else*/ 
-//   if (playerCard.cardNum === computerCard.cardNum) {
-//     if (!warTemp.includes(playerCard) && !warTemp.includes(computerCard)){
-//       warTemp.push(playerCard, computerCard);
-//     }
-//     console.log("equal");
-//     console.log(warTemp);
-//     for (let i = 0; i < 4; i++) {
-//       if (playerDeck.length === 0) {
-//         console.log("War Computer Win")
-//         console.log("computerDeck Loop" + computerDeck.length)
-//         gmWinner();
-//       } else if (computerDeck.length === 0) {
-//         console.log("War Player Win")
-//         gmWinner();
-//       } else { 
-//         playPlayerCard(true);
-//         playComputerCard(true);
-//       }
-//     }
-//     if (isGoing === false) {
-//       return;
-//     }
-//     compareCards();
-//   } else if (playerCard.cardNum > computerCard.cardNum) {
-//     console.log("player");
-//     if (warTemp.length > 0) {
-//       playerScore += Math.floor(warTemp.length / 2);
-//       computerScore -= Math.floor(warTemp.length / 2);
-//       playerDeck.push(...warTemp);
-//       warTemp = [];
-//     } else {
-//       playerScore++;
-//       computerScore--;
-//       playerDeck.push(computerCard);
-//       playerDeck.push(playerCard);
-//       if (computerDeck.length === 0) {
-//         console.log("Player Win")
-//         gmWinner();
-//       }
-//     }
-//     decRWinner.innerText = "Humans";
-//     console.log("Player: ", playerDeck.length);
-//     console.log("Computer: ", computerDeck.length);
-//     pScore.innerText = playerScore;
-//     cScore.innerText = computerScore;
-//   } else if (computerCard.cardNum > playerCard.cardNum) {
-//     console.log("computer");
-//     if (warTemp.length > 0) {
-//       playerScore -= Math.floor(warTemp.length / 2);
-//       computerScore += Math.floor(warTemp.length / 2);
-//       computerDeck.push(...warTemp);
-//       warTemp = [];
-//     } else {
-//       playerScore--;
-//       computerScore++;
-//       computerDeck.push(computerCard);
-//       computerDeck.push(playerCard);
-//     if (playerDeck.length === 0) {
-//       console.log("Computer Win")
-//       gmWinner();
-//     }
-//     }
-//     decRWinner.innerText = "Goblins";
-//     console.log("Player: ", playerDeck.length);
-//     console.log("Computer: ", computerDeck.length);
-//     cScore.innerText = computerScore;
-//     pScore.innerText = playerScore;
-//   }
-// }
-
-// compares the value of each card(WORKING) Cycles through decks once
 function compareCards() {
-    if (computerDeck.length === 0 || playerDeck.length === 0) {
-    gmWinner()
-  } else if (playerCard.cardNum === computerCard.cardNum) {
-    console.log('equal')
+  // if (computerDeck.length === 0 || playerDeck.length === 0) {
+  //   gmWinner()
+  /*} else*/ 
+  if (playerCard.cardNum === computerCard.cardNum) {
+    if (!warTemp.includes(playerCard) && !warTemp.includes(computerCard)){
+      warTemp.push(playerCard, computerCard);
+    }
+    console.log("equal");
+    console.log(warTemp);
     for (let i = 0; i < 4; i++) {
-      playPlayerCard();
-      playComputerCard();
+      if (playerDeck.length === 0) {
+        console.log("War Computer Win")
+        console.log("computerDeck Loop" + computerDeck.length)
+        gmWinner();
+      } else if (computerDeck.length === 0) {
+        console.log("War Player Win")
+        gmWinner();
+      } else { 
+        playPlayerCard(true);
+        playComputerCard(true);
+      }
+    }
+    if (isGoing === false) {
+      return;
     }
     compareCards();
   } else if (playerCard.cardNum > computerCard.cardNum) {
-    console.log('player')
-    playerScore++;
-    decRWinner.innerText = 'Humans';
+    console.log("player");
+    if (warTemp.length > 0) {
+      playerScore += Math.floor(warTemp.length / 2);
+      computerScore -= Math.floor(warTemp.length / 2);
+      playerDeck.push(...warTemp);
+      warTemp = [];
+    } else {
+      playerScore++;
+      computerScore--;
+      playerDeck.push(computerCard);
+      playerDeck.push(playerCard);
+      if (computerDeck.length === 0) {
+        console.log("Player Win")
+        gmWinner();
+      }
+    }
+    decRWinner.innerText = "Humans";
+    console.log("Player: ", playerDeck.length);
+    console.log("Computer: ", computerDeck.length);
     pScore.innerText = playerScore;
-  } else if (computerCard.cardNum > playerCard.cardNum){
-    console.log('computer')
-    computerScore++;
-    decRWinner.innerText = 'Goblins';
     cScore.innerText = computerScore;
+  } else if (computerCard.cardNum > playerCard.cardNum) {
+    console.log("computer");
+    if (warTemp.length > 0) {
+      playerScore -= Math.floor(warTemp.length / 2);
+      computerScore += Math.floor(warTemp.length / 2);
+      computerDeck.push(...warTemp);
+      warTemp = [];
+    } else {
+      playerScore--;
+      computerScore++;
+      computerDeck.push(computerCard);
+      computerDeck.push(playerCard);
+    if (playerDeck.length === 0) {
+      console.log("Computer Win")
+      gmWinner();
+    }
+    }
+    decRWinner.innerText = "Goblins";
+    console.log("Player: ", playerDeck.length);
+    console.log("Computer: ", computerDeck.length);
+    cScore.innerText = computerScore;
+    pScore.innerText = playerScore;
   }
-  console.log('Player: ', playerDeck.length)
-  console.log('Computer: ', computerDeck.length)
 }
+
+// // compares the value of each card(WORKING) Cycles through decks once
+// function compareCards() {
+//     if (computerDeck.length === 0 || playerDeck.length === 0) {
+//     gmWinner()
+//   } else if (playerCard.cardNum === computerCard.cardNum) {
+//     console.log('equal')
+//     for (let i = 0; i < 4; i++) {
+//       playPlayerCard();
+//       playComputerCard();
+//     }
+//     compareCards();
+//   } else if (playerCard.cardNum > computerCard.cardNum) {
+//     console.log('player')
+//     playerScore++;
+//     decRWinner.innerText = 'Humans';
+//     pScore.innerText = playerScore;
+//   } else if (computerCard.cardNum > playerCard.cardNum){
+//     console.log('computer')
+//     computerScore++;
+//     decRWinner.innerText = 'Goblins';
+//     cScore.innerText = computerScore;
+//   }
+//   console.log('Player: ', playerDeck.length)
+//   console.log('Computer: ', computerDeck.length)
+// }
 
 
 // Show Win/lose Message(WORKING)
@@ -295,7 +295,7 @@ function gmWinner() {
 
 // Play button function(Click-WORKING, Sounds-WORKING, Dealing-WORKING, Compare-WORKING)
 function playCards() {
-  // playSounds();
+  playSounds();
   playPlayerCard();
   playComputerCard();
   compareCards();
